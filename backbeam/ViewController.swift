c59da42d56f6e5084baf4d4a63b0b3406b552fc4
James Tang <j@jamztang.com>
2014-08-02 13:04:40 +0800

    Fix compilation error

--- backbeam/ViewController.swift
+++ backbeam/ViewController.swift
@@ -16,14 +16,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        request.secret = // replace with your secret
-        request.key = // replace with your key
+        request.secret = "<# your app's secret #>"
+        request.key = "<# your app's key #>"
         request.method = "GET"
         request.nonce = "6303cd9bf27d27eb6343427ac42365b38b09f112"
-        request.path = "/data/user"
+        request.path = "/data/user"   // customize to your entity
         request.time = "1397166331409"
         request.env = "dev"
-        request.project = // replace with your project
+        request.project = "<# your project #>"
         request.optionals["limit"] = 1
 
         request.send { (data, response, error) in
